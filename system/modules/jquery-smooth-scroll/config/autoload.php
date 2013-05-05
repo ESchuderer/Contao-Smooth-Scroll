229062f9e4836c7a5d4ae3e9a8eda72be525b96d
InitArt <info@initart.com>
2013-05-05 19:01:58 +0200

    release

---
+++ system/modules/jquery-smooth-scroll/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package Jquery_smoothscroll
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_smoothscroll' => 'system/modules/jquery-smooth-scroll/templates',
+));
---
+++ system/modules/jquery-smooth-scroll/languages/en/modules.php
@@ -0,0 +1,24 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package   jquery_smoothscroll 
+ * @author    Eike &#40;InitArt&#41; Schuderer 
+ * @license   MIT 
+ * @copyright jQuery Plugin Smooth Scroll - Karl Swedberg 2013, Contao Plugin - Eike Schuderer 2013 
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['jquery_smoothscroll'] = array('jQuery Smooth Scroll Plugin', '');
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['jquery_smoothscroll'] = array('jQuery Smooth Scroll Plugin', '');
